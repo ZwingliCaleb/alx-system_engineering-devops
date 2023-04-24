ee7b57329e7b16bf22e505e53ab93eb19d3005f4
ZwingliCaleb <calebzwingli@gmail.com>
2023-04-24 09:50:39 -0700

    create file in /tmp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,7 +2,8 @@
 
 
 file {'/tmp/school':
-content => 'I Love Puppet',
-owner 	=> 'www-data',
-group 	=> 'www-data',
-mode 	=> '0744'}
+  content => 'I love Puppet',
+  owner   => 'www-data',
+  group   => 'www-data',
+  mode    => '0744'
+}
