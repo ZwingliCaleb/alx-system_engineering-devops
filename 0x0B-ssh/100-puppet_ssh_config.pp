73a0f2eaf0a08b2cbd01ffb1c67b54e194b62c42
ZwingliCaleb <calebzwingli@gmail.com>
2023-05-03 02:55:44 -0700

    fresh fixes

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,14 @@
+# This is a Puppet file that is used to create changes to our configuration file
+
+include stdlib
+file_line { 'Turn off passwd auth':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
